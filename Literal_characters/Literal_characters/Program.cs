ecb7bae7730999ab6455853ceaafb3394bbc1acb
Mjeed Alshahrani <abdulmajidyahya@hotmail.com>
2018-09-30 23:30:44 +0300

    Literal _Characters
    This is Literal _Characters

---
+++ Literal_characters/Literal_characters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Literal_characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Myvar = -100;
+            uint myuvar = 1000u;
+            float myFLvar = 1.5F;
+            double myDvar = 3.5;
+            decimal mydecvar = 100.5M;
+            Console.WriteLine("{0} {1} ", Myvar, myuvar);
+            Console.Write($"(Myvar),(myuvar)");
+            Console.ReadKey();
+        }
+    }
+}
