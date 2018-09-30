8bae88955d82ea8a3ec21fdfffc411d9f3622d21
Mjeed Alshahrani <abdulmajidyahya@hotmail.com>
2018-09-30 22:37:20 +0300

    DataTybes-CharStringBooleanTybes
    This is a program to explain these Tybes in c#

---
+++ DataTybes_CharStringboolTybes/DataTybes_CharStringboolTybes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTybes_CharStringboolTybes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int myVarvalue = 'A';
+            int myvArvalue = 'a';
+            String mystring = "This is my character A value:";
+            String yourstring = "and This is my character a value:";
+            Console.WriteLine("{0} {1} {2} {3}", mystring, myVarvalue, yourstring, myvArvalue);
+                Console.ReadKey();
+;           
+            
+
+
+      
+            
+
+           
+        }
+
+    }
+}
