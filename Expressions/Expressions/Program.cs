d2015892571c9def14d85e0bf150907ff0449372
Mjeed Alshahrani <abdulmajidyahya@hotmail.com>
2018-10-02 19:32:14 +0300

    Expessions
    This is Expressions for math operations

---
+++ Expressions/Expressions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float myVar = 10.5F;
+            float myVar2 = 10.7F;
+
+            float temp = myVar + myVar2;
+
+            //  basic operations + - / * 
+            // bitwis operations 
+            // logical Boolean true , false
+
+
+            //1- unary operand 
+            //2- Binary operands 
+            //3- 3 operands 
+            
+
+        }
+    }
+}
