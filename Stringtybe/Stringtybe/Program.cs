d433c3e057b8c75387ce435ccbb11ce3f41b2f28
Mjeed Alshahrani <abdulmajidyahya@hotmail.com>
2018-10-02 19:26:04 +0300

    StringTybe
    This is string tybe

---
+++ Stringtybe/Stringtybe/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stringtybe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // \',\",\\,\0,\a,\b,\n,\r,\t,\v,\f,@
+            String mystring = "nazar said this is a good cours,and I think";
+            String mystring2 = @"yes it is";
+            String mystring3 = mystring + mystring2;
+            Console.WriteLine("{0}", mystring3);
+            Console.ReadKey(); 
+            
+        }
+    }
+}
