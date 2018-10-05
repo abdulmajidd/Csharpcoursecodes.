76a223c42154d529518d1214889e174fc1a2c618
Mjeed Alshahrani <abdulmajidyahya@hotmail.com>
2018-10-05 11:58:04 +0300

    increments - Decrements
    this is increments - Decrements

---
+++ increments-Decrements/increments-Decrements/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace increments_Decrements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//   اذا جات الاشارات قبل المتغير معناها نزود او ننقص واحد على حسب الي يجيني واذا جات بعدها تبقى قيمة المتغير نفسها 
+            int myvar = 2;
+            int temp = myvar--;
+            Console.WriteLine("{0}", temp );
+            Console.ReadKey();
+        }
+    }
+}
---
+++ increments-Decrements/increments-Decrements/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("increments-Decrements")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("increments-Decrements")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f5f24d1c-ff46-4e86-9cb1-305fee976c41")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
