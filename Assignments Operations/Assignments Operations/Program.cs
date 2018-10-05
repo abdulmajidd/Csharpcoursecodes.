7d4feffded82a7c9d94e08ce9d2228ae86116b99
Mjeed Alshahrani <abdulmajidyahya@hotmail.com>
2018-10-05 11:59:59 +0300

    Assignments Operations
    this is Assignments Operations

---
+++ Assignments Operations/Assignments Operations/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // =, +=, -=, *=, /=, %=,
+
+            int myvar1 = 7, myvar2 = 3;
+
+             myvar1 = myvar2;       
+             myvar1 += myvar2;     // like : myvar1 = myvar1 + myvar2 
+             myvar1 -= myvar2;     // like : myvar1 = myvar1 - myvar2 
+            myvar1 /= ++myvar2;      // like : myvar1 = myvar1 / ++myvar2  
+             myvar1 %= myvar2;          // like : myvar1 = myvar1 % myvar2 
+            Console.WriteLine("the resut 1 is {0} the value of var2  is {1}", myvar1, myvar2);
+            Console.ReadKey();
+        }
+    }
+}
