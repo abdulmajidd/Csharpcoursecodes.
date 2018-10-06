fe962d89449e39c27b8c117e03e9313fdc7db1fe
Mjeed Alshahrani <abdulmajidyahya@hotmail.com>
2018-10-06 18:05:33 +0300

    Boolean operators
    this is Boolean operators

---
+++ poolean Operators/poolean Operators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poolean_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Boolean comparison ==, !=, <, >, <=, >=                                         
+            // Boolean Conditions &&, ||                                                // && Table Truth 
+                                                                                        // F  F --> F 
+                                                                                        // F  T --> F
+                                                                                        // T  T --> T
+                                                                                        // T  F --> F                  
+            bool myresult;                                                                          
+            int myvar1 = 5, myvar2 = 8 ;
+            myresult = myvar1 != 8;
+            myresult = (myvar1 <= 5) && (myvar2 > 10);                                       
+            Console.WriteLine("myresult is = {0}", myresult );
+            Console.ReadKey();
+           
+        }
+    }
+}
